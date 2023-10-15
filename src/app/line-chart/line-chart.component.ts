1347e0859802752a48c705c73f982cf48bac38a0
WalterDoni <133245257+WalterDoni@users.noreply.github.com>
2023-10-15 23:19:27 +0200

    values for dashboard

--- src/app/line-chart/line-chart.component.ts
+++ src/app/line-chart/line-chart.component.ts
@@ -7,41 +7,69 @@ import { DocumentData, Firestore, collection, onSnapshot } from '@angular/fire/f
   styleUrls: ['./line-chart.component.scss']
 })
 export class LineChartComponent {
- 
+
   firestore: Firestore = inject(Firestore);
   users!: any[];
   dataValue!: number[];
+  labelValue = ["Gaming Grafikkarte", "Grafikkarte normal", "Curved 4k Monitor", "HDMI Monitor", "Mechanische Tastatur", "Silent Tastatur", "RGB Maus", "8GB Ram", "NextGen Prozessor", "Wasserkühlung RGB neu Model 2023"];
+  lineChartData: any;
+  unsubUsers;
 
- 
 
-  constructor(){
+  constructor() {
+    this.unsubUsers = this.subUser();
+  }
+
+  subUser() {
+    return onSnapshot(this.usersRef(), (list) => {
+      let currentUser: DocumentData[] = [];
+      list.forEach(element => {
+        currentUser.push({ data: element.data() });
+      });
+      this.users = currentUser;
+      this.getSalesPerProduct();
+      this.getChartData();
+    });
+  }
+
+  usersRef() {
+    return collection(this.firestore, 'users');
+  }
+
+  ngonDestroy() {
+    this.unsubUsers();
+  }
+
+  getSalesPerProduct() {
+
+    this.dataValue = [];
+    for (let i = 0; i < this.labelValue.length; i++) {
+      let value = 0;
+      for (let a = 0; a < this.labelValue.length; a++) {
+        value += this.users[a]['data']['amount'][i];
+      }
+      this.dataValue.push(value);
+    }
+    console.log(this.dataValue);
 
   }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-  lineChartData = {
-   labels: ["Gaming Grafikkarte","Grafikkarte normal","Curved 4k Monitor","HDMI Monitor","Mechanische Tastatur","Silent Tastatur","RGB Maus","8GB Ram","NextGen Prozessor","Wasserkühlung RGB neu Model 2023"],
-   datasets: [{
-    data: [15,45,33,20,44,60,33,81,10,63],
-    label: 'Product sales',
-    fill: true,
-    backgroundColor: 'green' ,
-    borderColor: 'black',
-    tension: 0.5,
-     }
-   ]
+getChartData(){
+  
+  this.lineChartData = {
+    labels: this.labelValue,
+    datasets: [{
+      data: this.dataValue,
+      label: 'Product sales',
+      fill: true,
+      backgroundColor: 'green',
+      borderColor: 'black',
+      tension: 0.5,
+    }
+    ]
   }
+}
 
 }
