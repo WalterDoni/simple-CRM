5dd337fcd9841bc0dfbf9b75d80399264cb543b2
WalterDoni <133245257+WalterDoni@users.noreply.github.com>
2023-10-17 17:27:20 +0200

    save changes

--- src/app/dialog-edit-user-name-email/dialog-edit-user-name-email.component.ts
+++ src/app/dialog-edit-user-name-email/dialog-edit-user-name-email.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { Firestore, collection, doc, updateDoc } from '@angular/fire/firestore';
 import { MatDialogRef } from '@angular/material/dialog';
 import { User } from 'src/models/user.class';
+import { UserDetailComponent } from '../user-detail/user-detail.component';
 
 @Component({
   selector: 'app-dialog-edit-user-name-email',
@@ -16,7 +17,7 @@ export class DialogEditUserNameEmailComponent {
   firestore: Firestore = inject(Firestore)
 
 
-  constructor(public dialogRef: MatDialogRef<DialogEditUserNameEmailComponent>) { }
+  constructor(public dialogRef: MatDialogRef<DialogEditUserNameEmailComponent>, private edit: UserDetailComponent) { }
 
   dialogClose() {
     this.dialogRef.close();
@@ -26,6 +27,7 @@ export class DialogEditUserNameEmailComponent {
     this.loading = true;
     let selectedUser = collection(this.firestore, 'users')
     await updateDoc(doc(selectedUser, this.userId), this.userData.toJSON());
+    this.edit.subUserDetail();
     this.loading = false;
     this.dialogRef.close();
   }
