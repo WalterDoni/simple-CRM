6c61a5c30a2a37fa8dc84f4e6910e7491df58524
WalterDoni <133245257+WalterDoni@users.noreply.github.com>
2023-10-21 15:16:14 +0200

    responsive menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,21 @@ import { LoginComponent } from './login/login.component';
 })
 export class AppComponent {
   title = 'simple-crm';
+  hideResponsive: boolean = true;
 
 
   openLogInWIndow(){
     LoginComponent.showLoginWindow = false;
+  }
+
+  toggleAnimation(){
+    let container = document.getElementById('responsive-container')
+    if(this.hideResponsive == true){
+      container?.classList.add('responsive-cotainer-open-animation')
+    }else if(this.hideResponsive == false){
+      container?.classList.add('responsive-cotainer-close-animation')
+    }
+    this.hideResponsive = !this.hideResponsive
   }
 }
 
