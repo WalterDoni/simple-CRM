e8bc96c301985990f40936a90eae4c0a4d35482a
WalterDoni <133245257+WalterDoni@users.noreply.github.com>
2023-12-23 14:53:51 +0100

    fix

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,5 +1,5 @@
-import { Component, inject } from '@angular/core';
-import { Firestore, collection, doc, collectionData, onSnapshot, addDoc } from '@angular/fire/firestore';
+import { Component, ElementRef, ViewChild, inject } from '@angular/core';
+import { Firestore, collection, onSnapshot, addDoc } from '@angular/fire/firestore';
 import { MatDialogRef } from '@angular/material/dialog';
 import { User } from 'src/models/user.class';
 
@@ -9,7 +9,9 @@ import { User } from 'src/models/user.class';
   styleUrls: ['./dialog-add-user.component.scss']
 })
 export class DialogAddUserComponent {
-  
+
+  @ViewChild('companyName') companyName!: ElementRef;
+
   user = new User();
   loading = false;
   firestore: Firestore = inject(Firestore);
@@ -19,41 +21,31 @@ export class DialogAddUserComponent {
     this.unsubUsers = this.subUsers();
   }
 
-  //----Subscribe-Functions-> Firebase----//
-
-  subUsers() {
-    return onSnapshot(this.usersRef(), (list) => {
-      list.forEach(element => {
-        //console.log(element.data());
-      });
-    })
-  }
-
-  ngonDestroy() {
-    this.unsubUsers();
-  }
-
-  usersRef() {
-    return collection(this.firestore, 'users');
-  }
-
   //----Save new User----//
   /**
    * At first get the current date. After that a short load animation will appear at the top of the box. During this time the data get changed to a JSON before it will be pushed in the Firestore database.
    * If something went wrong, it will appear in the console. The setTimeout will stop the loading animation and close the window.
    */
   async saveNewUser() {
-    this.currentDate();
-    this.loading = true;
-    const userData = this.user.toJSON();
-    await addDoc(this.usersRef(), userData).catch(
-      (err) => { console.error(err); }
-    ).then(
-      () => setTimeout(() => {
-        this.loading = false;
-        this.dialogRef.close();
-      }, 500)
-    )
+    if (this.user.company.length <= 0) {
+      alert('Please, add the name of the company.');
+      return
+    } else if (this.user.email.length <= 0) {
+      alert('Please, add the email-adress of the company.');
+    }
+    else {
+      this.currentDate();
+      this.loading = true;
+      const userData = this.user.toJSON();
+      await addDoc(this.usersRef(), userData).catch(
+        (err) => { console.error(err); }
+      ).then(
+        () => setTimeout(() => {
+          this.loading = false;
+          this.dialogRef.close();
+        }, 500)
+      )
+    }
   }
 
   //----Date----//
@@ -72,6 +64,24 @@ export class DialogAddUserComponent {
   dialogClose() {
     this.dialogRef.close();
   }
+
+  //----Subscribe-Functions-> Firebase----//
+
+  subUsers() {
+    return onSnapshot(this.usersRef(), (list) => {
+      list.forEach(element => {
+      });
+    })
+  }
+
+  ngonDestroy() {
+    this.unsubUsers();
+  }
+
+  usersRef() {
+    return collection(this.firestore, 'users');
+  }
+
 
 
 }
