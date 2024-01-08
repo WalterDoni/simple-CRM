c11a48fbd4504855644cbe4affff385d79447df0
WalterDoni <133245257+WalterDoni@users.noreply.github.com>
2024-01-08 15:21:42 +0100

    fix max value

--- src/app/add-sales/add-sales.component.ts
+++ src/app/add-sales/add-sales.component.ts
@@ -26,26 +26,30 @@ export class AddSalesComponent {
   }
 
   async addAmountToSelectedUserAndProduct() {
-    let selectedUser = collection(this.firestore, 'users');
-    for (let i = 0; i < this.allUsers.length; i++) {
-      let currentUser = this.allUsers[i]['data'];
-      let currentID = this.allUsers[i]['id']
-      if (currentUser['company'] == this.selectedCompany) {
-        for (let p = 0; p < this.productNames.length; p++) {
-       
-          let currentName = this.productNames[p];
-          if (currentName == this.selectedProduct) {
-            let newAmountArray = [...this.allUsers[i]['data']['amount']];
-            newAmountArray[p] = newAmountArray[p] + this.selectedAmount;
-             await updateDoc(doc(selectedUser, currentID),{
-              amount: newAmountArray,
-             })
+    if (isNaN(Number(this.selectedAmount)) || Number(this.selectedAmount) < 0 || Number(this.selectedAmount) > 99) {
+      alert('Invalid value: Please insert a number between 0 and 99.');
+      return;
+    } else {
+      let selectedUser = collection(this.firestore, 'users');
+      for (let i = 0; i < this.allUsers.length; i++) {
+        let currentUser = this.allUsers[i]['data'];
+        let currentID = this.allUsers[i]['id']
+        if (currentUser['company'] == this.selectedCompany) {
+          for (let p = 0; p < this.productNames.length; p++) {
+            let currentName = this.productNames[p];
+            if (currentName == this.selectedProduct) {
+              let newAmountArray = [...this.allUsers[i]['data']['amount']];
+              newAmountArray[p] = newAmountArray[p] + this.selectedAmount;
+              await updateDoc(doc(selectedUser, currentID), {
+                amount: newAmountArray,
+              })
+            }
           }
         }
       }
     }
 
-  }  
+  }
 
   dialogClose() {
     this.dialogRef.close();
